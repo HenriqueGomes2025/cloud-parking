1ff94301937758fd569f4bbca121169aa7d212a9
HenriqueGomes2025 <henriquefigueredo56@gmail.com>
2025-10-29 15:11:15 -0300

    Primeiro endpoint de lista

---
+++ src/main/java/one/digialinnovation/parking/controller/ParkingController.java
@@ -0,0 +1,34 @@
+package one.digialinnovation.parking.controller;
+
+import one.digialinnovation.parking.dto.ParkingDTO;
+import one.digialinnovation.parking.mapper.ParkingMapper;
+import one.digialinnovation.parking.model.Parking;
+import one.digialinnovation.parking.service.ParkingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+    
+    private final ParkingService parkingService;
+    private final ParkingMapper parkingMapper;
+    
+    public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper) {
+        this.parkingService = parkingService;
+        this.parkingMapper = parkingMapper;
+    }
+    
+    
+    @GetMapping
+    public List<ParkingDTO> findAll() {
+        List<Parking> parkingList = parkingService.findAll();
+        List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
+        return result;
+    }
+}
